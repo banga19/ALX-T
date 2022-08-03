0d16a04384f6f0a24db3c3baf2cdaf77e656ab32
banga19 <bangalifof16gmail.com>
2022-08-03 21:49:07 +0300

    tried to query the db in python3 interactive shell

--- Users.py
+++ Users.py
@@ -1,19 +1,21 @@
-from typing_extensions import Self
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
+# setting up th db connection
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://banga:banga123@localhost:5432/example'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+## models 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
-    def __repr__(Self):
-        return f'user {Self.id}, {Self.name}'
+    ## enables debuging on python3 interactive shell
+    def __repr__(self):
+        return f'<User id: {self.id}, Name: {self.name}>'
 
 db.create_all()
