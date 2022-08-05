ec29a8932ffab18f8628cfc7c3cce9a594240fd4
banga19 <bangalifof16gmail.com>
2022-08-06 02:09:21 +0300

    added the migrations/ folder after running flask-db-init on the TERMINAL

--- todoapp/app.py
+++ todoapp/app.py
@@ -31,4 +31,4 @@ class Todo(db.Model):
 # FLASK-SETUP This code should be at the bottom of all your files.
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3001)
